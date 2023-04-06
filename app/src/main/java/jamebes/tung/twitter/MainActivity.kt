cf1f0ffc2b1fe0c7ffd7a73f402044fbdc9239d5
Tung-Home <dctung249@gmail.com>
2023-04-06 22:31:39 +0700

    update text

--- app/src/main/java/jamebes/tung/twitter/MainActivity.kt
+++ app/src/main/java/jamebes/tung/twitter/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -13,6 +14,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.BaselineShift
@@ -87,7 +90,6 @@ fun TwitterLoginPage() {
             )
 
 
-
             Button(
                 onClick = {
 
@@ -139,7 +141,8 @@ fun TwitterLoginPage() {
             )
             Text(
                 text = " Or ",
-                color = Color.DarkGray
+                color = Color.DarkGray,
+                fontSize = 12.sp
             )
             Divider(
                 color = Color.Gray,
@@ -157,62 +160,71 @@ fun TwitterLoginPage() {
             Text("Create account")
         }
 
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(bottom = 8.dp)
+        Column(
+            modifier = Modifier.padding(start = 35.dp, top = 16.dp)
         ) {
-            Text(text = "By signing up, you agree to the",
-                fontSize = 12.sp,
-                color = Color.Gray
-            )
-            TextButton(
-                onClick = { /* your onClick code */ },
-            ) {
-                Text("Terms of Service",
-                    fontSize = 12.sp,
-                color =  Color(0xFF00CCFF))
-            }
-            Text(text = "and",
-                fontSize = 12.sp,
-                color = Color.Gray,
-            )
+            val termsOfService = "Terms of Service"
+            val privacyPolicy = "Privacy Policy"
+            val cookieUse = "Cookie use"
+            val message =
+                "By signing up, you agree to the $termsOfService and $privacyPolicy, including $cookieUse"
 
-        }
+            val annotatedString = AnnotatedString.Builder(message)
+                .apply {
+                    val startIndex = message.indexOf(termsOfService)
+                    if (startIndex >= 0) {
+                        val endIndex = startIndex + termsOfService.length
+                        addStyle(SpanStyle(color = Color(0xFF00CCFF)), startIndex, endIndex)
+                    }
 
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(bottom = 16.dp)
-        ) {
-            TextButton(
-                onClick = { /* your onClick code */ },
-            ) {
-                Text("Privacy Policy",
-                    fontSize = 12.sp,
-                    color =  Color(0xFF00CCFF),
-                    modifier = Modifier.padding(bottom = 60.dp)
+                    val secondIndex = message.indexOf(privacyPolicy)
+                    if (secondIndex >= 0) {
+                        val endIndex = secondIndex + privacyPolicy.length
+                        addStyle(SpanStyle(color = Color(0xFF00CCFF)), secondIndex, endIndex)
+                    }
+
+                    val thirdIndex = message.indexOf(cookieUse)
+                    if (thirdIndex >= 0) {
+                        val endIndex = thirdIndex + cookieUse.length
+                        addStyle(SpanStyle(color = Color(0xFF00CCFF)), thirdIndex, endIndex)
+                    }
+                }
+                .toAnnotatedString()
+            Text(text = annotatedString,
+               style = TextStyle(
+                   fontSize = 13.sp,
+                   color = Color.Gray
+               )
                 )
-
-            }
-            Text(text = ", including",
-                fontSize = 12.sp,
-                color = Color.Gray)
-            TextButton(
-                onClick = { /* your onClick code */ },
-            ) {
-                Text("Cookie Use.",
-                    fontSize = 12.sp,
-                    color =  Color(0xFF00CCFF))
-            }
         }
 
+        
 
-
-
+        val login = "Log in"
+        val haveAccount = "Have an account already? $login"
+        val loginAnnotatedString = AnnotatedString.Builder(haveAccount)
+            .apply {
+                val startIndex = haveAccount.indexOf(login)
+                if (startIndex >= 0) {
+                    val endIndex = startIndex + login.length
+                    addStyle(SpanStyle(color = Color(0xFF00CCFF)), startIndex, endIndex)
+                }
+            }.toAnnotatedString()
+            Text(text = loginAnnotatedString,
+            style = TextStyle(
+                fontSize = 16.sp,
+                color = Color(0xFF5f5f5f)
+            ),
+                modifier = Modifier.align(Alignment.Start)
+                    .padding(start = 35.dp, top = 40.dp)
+                )
 
     }
 
 
 }
+
+
 
 @Composable
 fun Test(){
